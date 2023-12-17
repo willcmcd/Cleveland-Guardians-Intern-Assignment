552e1b168134424a72741cd2d20fb17040b6c617
willcmcd <106502129+willcmcd@users.noreply.github.com>
2023-12-16 17:44:26 -0800

    Restructure Team Components

---
+++ vue-project/src/components/TeamList.vue
@@ -0,0 +1,40 @@
+<template>
+    <!-- Generate list of all teams -->
+    <TeamListItem v-for="team in teams" 
+        :key="team.id" 
+        :team-name="team.teamName" 
+        :team-id="team.id" 
+        :team-abbrv="team.fileCode"
+        :venue="team.venue.name"
+        :city="team.locationName">
+    </TeamListItem>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TeamListItem from "@/components/TeamListItem.vue";
+import axios from "axios";
+
+export default defineComponent({  
+  name: 'TeamList',
+  components: {
+    TeamListItem,
+  },
+  data() {
+    return {
+      teams: null
+    }
+  },
+  mounted() {
+    this.LoadTeams();
+  },
+  methods: {
+    async LoadTeams() {
+      const response = await axios.get("https://statsapi.mlb.com/api/v1/teams", { params: { activeStatus: true , sportId: 1 } });
+      const res = await response.data;
+      this.teams = res.teams;
+    },
+  }
+});
+
+</script>
---
+++ vue-project/src/components/TeamListItem.vue
@@ -0,0 +1,23 @@
+<template>
+        <div>
+            <h1>{{ teamName }}</h1>
+            <img :src="'https://a.espncdn.com/combiner/i?img=/i/teamlogos/mlb/500/scoreboard/' + teamAbbrv + '.png'" alt="TeamLogo" width="50px">
+            <p>The {{ city }} {{ teamName }}'s play at their home field, {{ venue }}</p>
+        </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+// import axios from "axios";
+
+export default defineComponent({
+    name: 'TeamListItem',
+    props: {
+        teamId: String,
+        teamName: String,
+        teamAbbrv: String,
+        venue: String,
+        city: String,
+    }
+});
+</script>
