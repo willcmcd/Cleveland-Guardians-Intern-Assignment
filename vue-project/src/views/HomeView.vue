abb1a2a6f1e9e1ceee23080badc11f00d86ece59
willcmcd <106502129+willcmcd@users.noreply.github.com>
2023-12-19 17:48:44 -0800

    New Home to show of implemented features
    Added menu list to send users to highlight pages.

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="bg-light text-4xl text-dark text-center flex items-center justify-center min-h-screen">
+    <div class="flex flex-col bg-primary rounded">
+      <div class="p-4 my-2 rounded">
+        <p class="text-4xl">MENU</p>
+      </div>
+      <router-link to="/games">
+      <div class="bg-secondary text-primary hover:bg-pop hover:text-dark transition duration-200 p-2 mx-2 my-1 rounded">
+        <p>Schedule</p>
+      </div>
+      </router-link>
+      <router-link to="/teams">
+      <div class="bg-secondary text-primary hover:bg-pop hover:text-dark transition duration-200 p-2 mx-2 my-1 rounded">
+        <p>Team List</p>
+      </div>
+      </router-link>
+      <router-link to="/team/114">
+      <div class="bg-secondary text-primary hover:bg-pop hover:text-dark transition duration-200 p-2 mx-2 my-1 rounded">
+        <p>Team Spotlight</p>
+      </div>
+      </router-link>
+    </div>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
